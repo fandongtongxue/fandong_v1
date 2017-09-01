f08bd90f92ac08a6b494b3baa06b3cee9250bab8
范东 <admin@fandong.me>
2017-09-01 11:38:59 +0800

    修改注册接口创建登录接口

--- Sources/App/Routes.swift
+++ Sources/App/Routes.swift
@@ -5,7 +5,7 @@ extension Droplet {
     func setupRoutes() throws {
         //1.用户相关
         //1.1注册用户
-        get("registerUser"){ req in
+        get("userRegister"){ req in
             //获取用户名和密码
             let username = req.data["userName"]
             let password = req.data["passWord"]
@@ -25,20 +25,39 @@ extension Droplet {
                 try mysqlDriver.raw(insertMysqlStr)
                 let excuteResult = try mysqlDriver.raw("select * from app_user where userName='" + (username?.string)! + "';")
                 let userinfo = excuteResult[0]
-                if userinfo == nil{
-                    return try JSON(node: [
-                        "data":"",
-                        "msg" : "注册失败",
-                        "state":0
-                        ])
-                }else{
+                if userinfo != nil{
                     return try JSON(node: [
                         "data":"",
                         "msg" : "注册成功",
                         "state":1
                         ])
+                    
                 }
+                return try JSON(node: [
+                    "data":"",
+                    "msg" : "注册失败",
+                    "state":0
+                    ])
             }
+        }
+        //1.2用户登录
+        get("userLogin"){ req in
+            //获取用户名和密码
+            let username = req.data["userName"]
+            let password = req.data["passWord"]
+            //创建MySQL驱动
+            let mysqlDriver = try self.mysql()
+            //查询是否已存在此用户
+            let result = try mysqlDriver.raw("select * from app_user where userName='" + (username?.string)! + "';")
+            if result[0] != nil{
+                //判断密码是否正确
+                return "需要判断密码是否正确"
+            }
+            return try JSON(node: [
+                "data":"",
+                "msg" : "用户不存在,请先注册",
+                "state":0
+                ])
         }
     }
 }
