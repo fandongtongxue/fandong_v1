552e2802cc91138d31dd52069805e21dbdf2d901
范东 <admin@fandong.me>
2017-09-01 10:53:14 +0800

    注册接口走通

--- Sources/App/Routes.swift
+++ Sources/App/Routes.swift
@@ -7,12 +7,12 @@ extension Droplet {
         //1.1注册用户
         get("registerUser"){ req in
             //获取用户名和密码
-            let username = req.data["username"]
-            let password = req.data["password"]
+            let username = req.data["userName"]
+            let password = req.data["passWord"]
             //创建MySQL驱动
             let mysqlDriver = try self.mysql()
             //查询是否已存在此用户
-            let result = try mysqlDriver.raw("select * from app_user where username='" + (username?.string)! + "';")
+            let result = try mysqlDriver.raw("select * from app_user where userName='" + (username?.string)! + "';")
             if result[0] != nil{
                 return try JSON(node: [
                     "data":"",
@@ -21,15 +21,21 @@ extension Droplet {
                     ])
             }else{
                 //用户写入数据库
-                let insertMysqlStr = String.init(format: "INSERT INTO app_user(id,username,password) VALUES(1,'%@','%@');", (username?.string)!,(password?.string)!)
+                let insertMysqlStr = String.init(format: "INSERT INTO app_user(uid,userName,passWord) VALUES(0,'%@','%@');", (username?.string)!,(password?.string)!)
                 let insertResult = try mysqlDriver.raw(insertMysqlStr)
-                let excuteResult = try mysqlDriver.raw("select * from app_user where username='" + (username?.string)! + "';")
+                let excuteResult = try mysqlDriver.raw("select * from app_user where userName='" + (username?.string)! + "';")
                 let userinfo = excuteResult[0]
-                if userinfo != nil{
+                if userinfo == nil{
                     return try JSON(node: [
                         "data":"",
                         "msg" : "注册失败",
                         "state":0
+                        ])
+                }else{
+                    return try JSON(node: [
+                        "data":"",
+                        "msg" : "注册成功",
+                        "state":1
                         ])
                 }
             }
