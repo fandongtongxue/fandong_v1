e1e197ffffb220da2e50fe00be83a945e7da2e60
范东 <admin@fandong.me>
2017-09-01 11:12:33 +0800

    消除警告,注册sql语句

--- Sources/App/Routes.swift
+++ Sources/App/Routes.swift
@@ -21,8 +21,8 @@ extension Droplet {
                     ])
             }else{
                 //用户写入数据库
-                let insertMysqlStr = String.init(format: "INSERT INTO app_user(uid,userName,passWord) VALUES(0,'%@','%@');", (username?.string)!,(password?.string)!)
-                let insertResult = try mysqlDriver.raw(insertMysqlStr)
+                let insertMysqlStr = "INSERT INTO app_user(uid,userName,passWord) VALUES(0,'" + (username?.string)! + "','" +  (password?.string)! + "');"
+                try mysqlDriver.raw(insertMysqlStr)
                 let excuteResult = try mysqlDriver.raw("select * from app_user where userName='" + (username?.string)! + "';")
                 let userinfo = excuteResult[0]
                 if userinfo == nil{
