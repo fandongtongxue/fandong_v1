1b356cad5f01d1982baa6ce4d2f55aad5a257d54
范东 <admin@fandong.me>
2017-09-05 12:02:24 +0800

    增加修改密码/获取用户信息功能

--- Sources/App/Routes.swift
+++ Sources/App/Routes.swift
@@ -1,6 +1,10 @@
 import Vapor
 import MySQLProvider
 
+let USER_DEFAULT_ICON = "http://ov2uvg3mg.bkt.clouddn.com/USER_DEFAULT_ICON.jpg"
+let USER_DEFAULT_INTRODUCE = "Nothing to say"
+
+
 extension Droplet {
     func setupRoutes() throws {
         //1.用户相关
@@ -40,6 +44,9 @@ extension Droplet {
                 let excuteResult = try mysqlDriver.raw("select * from app_user where userName='" + (userName?.string)! + "';")
                 let userinfo = excuteResult[0]
                 if userinfo != nil{
+                    //写入用户信息表
+                    let insertMysqlUserInfoStr = "INSERT INTO app_userInfo(id,uid,icon,nickName,introduce) VALUES(0,'" + (userinfo?.wrapped["uid"]?.string)!+"','"+USER_DEFAULT_ICON+"','"+(userName?.string)!+"','"+USER_DEFAULT_INTRODUCE+"');"
+                    try mysqlDriver.raw(insertMysqlUserInfoStr)
                     return try JSON(node: [
                         "data":"",
                         "msg" : "注册成功",
@@ -87,8 +94,8 @@ extension Droplet {
                 let getPassWordHash = try drop.hash.make((passWord?.string)!)
                 let getPassWordHashStr = getPassWordHash.makeString()
                 //MySQL中密码的哈希值
-                let resultNode = result[0]!
-                let resultStructuredData = resultNode.wrapped 
+                let resultNode = result[0] as! Node
+                let resultStructuredData = resultNode.wrapped as! StructuredData
                 let mysqlPasswordStr = (resultStructuredData["passWord"]?.string)!
                 let mysqlPasswordHash = try drop.hash.make(mysqlPasswordStr)
                 let mysqlPasswordHashStr = mysqlPasswordHash.makeString()
@@ -109,6 +116,104 @@ extension Droplet {
             return try JSON(node: [
                 "data":"",
                 "msg" : "用户不存在,请先注册",
+                "state":0
+                ])
+        }
+        //1.3已知密码修改密码
+        get("userChangePassWord"){ req in
+            //获取GET数据
+            let userName = req.data["userName"]
+            let oldPassWord = req.data["oldPassWord"]
+            let newPassWord = req.data["newPassWord"];
+            if userName == nil || userName == ""{
+                return try JSON(node: [
+                    "data":"",
+                    "msg" : "用户名为空",
+                    "state":0
+                    ])
+            }
+            if oldPassWord == nil || oldPassWord == ""{
+                return try JSON(node: [
+                    "data":"",
+                    "msg" : "原密码为空",
+                    "state":0
+                    ])
+            }
+            if newPassWord == nil || newPassWord == ""{
+                return try JSON(node: [
+                    "data":"",
+                    "msg" : "新密码为空",
+                    "state":0
+                    ])
+            }
+            //判断原密码是否正确
+            //创建MySQL驱动
+            let mysqlDriver = try self.mysql()
+            //查询是否已存在此用户
+            let result = try mysqlDriver.raw("select * from app_user where userName='" + (userName?.string)! + "';")
+            if result[0] != nil{
+                //判断密码是否正确
+                //创建Drop
+                let config = try Config()
+                try config.setup()
+                let drop = try Droplet(config)
+                //GET中的密码哈希值
+                let getPassWordHash = try drop.hash.make((oldPassWord?.string)!)
+                let getPassWordHashStr = getPassWordHash.makeString()
+                //MySQL中密码的哈希值
+                let resultNode = result[0] as! Node
+                let resultStructuredData = resultNode.wrapped as! StructuredData
+                let mysqlPasswordStr = (resultStructuredData["passWord"]?.string)!
+                let mysqlPasswordHash = try drop.hash.make(mysqlPasswordStr)
+                let mysqlPasswordHashStr = mysqlPasswordHash.makeString()
+                //密码验证
+                if getPassWordHashStr != mysqlPasswordHashStr {
+                    return try JSON(node: [
+                        "data":"",
+                        "msg" : "原密码错误",
+                        "state":0
+                        ])
+                }
+                //覆盖原密码
+                let updateMysqlStr = "UPDATE app_user SET passWord = '" + (newPassWord?.string)! + "' WHERE userName = '" + (userName?.string)! + "';"
+                try mysqlDriver.raw(updateMysqlStr)
+                return try JSON(node: [
+                    "data":"",
+                    "msg" : "修改密码成功",
+                    "state":1
+                    ])
+            }
+            //正确的话覆盖原密码
+            return try JSON(node: [
+                "data":"",
+                "msg" : "用户不存在,请先注册",
+                "state":0
+                ])
+        }
+        //1.4获取用户信息
+        get("userInfo"){ req in
+            let uid = req.data["uid"]
+            if uid == nil || uid == ""{
+                return try JSON(node: [
+                    "data":"",
+                    "msg" : "uid为空",
+                    "state":0
+                    ])
+            }
+            //创建MySQL驱动
+            let mysqlDriver = try self.mysql()
+            //查询是否已存在此用户
+            let result = try mysqlDriver.raw("select * from app_userInfo where uid='" + (uid?.string)! + "';")
+            if result[0] != nil{
+                return try JSON(node: [
+                    "data":["userInfo":JSON(result)],
+                    "msg" : "获取用户信息成功",
+                    "state":1
+                    ])
+            }
+            return try JSON(node: [
+                "data":"",
+                "msg" : "获取用户信息失败",
                 "state":0
                 ])
         }
